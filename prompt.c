f89e7725c8681441cd583b0a0cb991540cebca8e
Mariam <kelanimaryam1234@gmail.com>
2023-11-14 23:52:14 -0800

    all files

---
+++ prompt.c
@@ -0,0 +1,37 @@
+#include "shell.h"
+/**
+ * main - Entry point of program
+ *
+ * Return: 0 on sucess
+ */
+int main(void)
+{
+	char *line_ptr = NULL;
+	size_t bufsize = 0;
+	ssize_t readlines;
+	unsigned int i;
+
+	/*char *argv[] = {"bin/ls", "-l", "/tmp", NULL};*/
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			printf("$ ");
+		readlines = getline(&line_ptr, &bufsize, stdin);
+
+		if (readlines == -1)
+		{
+			perror("getline");
+			free(line_ptr);
+			exit(EXIT_FAILURE);
+		}
+		if (readlines > 0 && line_ptr[readlines - 1] == '\n')
+		{
+			line_ptr[readlines - 1] = '\0';
+		}
+		execute_command(line_ptr);
+	}
+	free(line_ptr);
+
+	return (0);
+}
---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <signal.h>
+
+#endif
